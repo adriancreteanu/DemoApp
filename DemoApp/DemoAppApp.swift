fc88f795ca15d12d4ce2fed17b1e57964ec5b970
Adrian Creteanu <creteanu.adrian@gmail.com>
2022-08-05 16:19:39 +0300

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Adrian Creteanu on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
